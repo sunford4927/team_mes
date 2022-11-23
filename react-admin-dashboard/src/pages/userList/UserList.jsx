bbdc4193b4c6b275e79f925f54e78d88ba1619e8
sunford4927 <sunford4927@gmail.com>
2022-11-24 00:22:10 +0900

    Lee commit

--- react-admin-dashboard/src/pages/userList/UserList.jsx
+++ react-admin-dashboard/src/pages/userList/UserList.jsx
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import "./userList.css";
 import { createUseGridApiEventHandler, DataGrid } from "@mui/x-data-grid";
-import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined";
-import { userRows } from "../../dummyData";
 import ManageMent from "../management/ManageMent";
 import Product from "../product/Product";
 import axios from "axios";
-// import { FormatListNumbered } from "@material-ui/icons";
 
 const UserList = () => {
   const [dummyData, setDummyData] = useState("");
@@ -16,53 +12,30 @@ const UserList = () => {
     for (var i = 0; i < dummyData.length; i++) {
       dummyData[i]["id"] = i;
     }
-    // console.log(dummyData);
     return dummyData;
   }
-  function Make_Table(plans, orders, customers) {
-    var plans_order = [];
-    var order_customer = [];
-    var customer_name = [];
-    var order_full = []
-
-    console.log(plans)
-    // console.log(orders)
-    // console.log(customers)
+  function Make_Table(plans, orders, customers, items) {
     for (var i = 0; i < plans.length; i++) {
-        for (var j = 0; j< orders.lengt; j++) {
-            if(plans[i]['order_code'] == orders[j]['order_code']){
-                plans[i]['customer_code'] = orders[j]['customer_code'];
-            }
-            for (var k = 0; k < customers; k++){
-                if (plans[i]['customer_code'] == customers[k]['customer_code']){
-                    plans[i]['customer_name'] = customers[k]['customer_name']
-                }
-            }
-
-
+      for (var j = 0; j < orders.length; j++) {
+        if (plans[i]["order_code"] == orders[j]["order_code"]) {
+          plans[i]["customer_code"] = orders[j]["customer_code"];
         }
+        for (var k = 0; k < customers.length; k++) {
+          if (plans[i]["customer_code"] == customers[k]["customer_code"]) {
+            plans[i]["customer_name"] = customers[k]["customer_name"];
+          }
+        }
+      }
     }
 
-console.log(plans)
-    // console.log(plans_order.includes("CC-001")==false);
-    // for (var j = 0; j < orders.length; j++) {
-    //   if (plans_order.includes(orders[j]["order_code"]) == true) {
-    //     order_customer.push(orders[j]["customer_code"]);
-    //     order_full.push(orders[j]);
-
-    //   };
-    //   // console.log(orders[j]["order_code"]);
-    // };
-    // console.log(order_customer);
-
-    // for (var k = 0; k < customers.length; k++) {
-    //   if (order_customer.includes(customers[k]["customer_code"]) == true) {
-    //     customer_name.push(customers[k]);
-    //   };
-    // };
-    // console.log(order_full)
-    // console.log(customer_name);
-    // return customer_name;
+      for (var u = 0; u < plans.length; u++) {
+        for (var y = 0; y < items.length; y++) {
+          if (plans[u]["item_code"] == items[y]["item_code"]) {
+            plans[u]["item_name"] = items[y]["item_name"];
+          }
+        }
+      }
+      console.log(plans)
   }
 
   const columns = [
@@ -73,8 +46,8 @@ console.log(plans)
       width: 160,
     },
     { field: "order_code", headerName: "수주코드", width: 140 },
-    // { field: "고객명", headerName: "고객명", width: 160 },
-    // { field: "제품명", headerName: "제품명", width: 160 },
+    { field: "customer_name", headerName: "고객명", width: 160 },
+    { field: "item_name", headerName: "제품명", width: 160 },
     { field: "quantity", headerName: "수량", width: 70 },
     { field: "due_date", headerName: "생산완료예정일", width: 150 },
     { field: "reg_date", headerName: "생산계획등록일", width: 150 },
@@ -85,18 +58,20 @@ console.log(plans)
       try {
         const result = await axios.get("http://127.0.0.1:8000/plans/");
         const result_data1 = await axios.get("http://127.0.0.1:8000/orders/");
-        const result_data2 = await axios.get("http://127.0.0.1:8000/customers/");
+        const result_data2 = await axios.get(
+          "http://127.0.0.1:8000/customers/"
+        );
+        const result_data3 = await axios.get("http://127.0.0.1:8000/items/");
         setDummyData(Make_ID(result.data));
-        setOrders(Make_Table(result.data, result_data1.data ,result_data2.data));
+        setOrders(
+          Make_Table(result.data, result_data1.data, result_data2.data, result_data3.data)
+        );
       } catch (error) {
         console.error(error);
       }
     };
-Plansgetdata();
+    Plansgetdata();
   }, []);
-  console.log(dummyData)
-  //   console.log(dummyData);
-//   console.log(orders);
   return (
     <div className="userList">
       <ManageMent />
