e1aa437b0f3336b395fe93c0aad99d5aad1e100f
chaeyunyoung <sdfnkl12@gmail.com>
2022-11-24 10:18:04 +0900

    hi

--- react-admin-dashboard/src/pages/monitoring/MoniToring.jsx
+++ react-admin-dashboard/src/pages/monitoring/MoniToring.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import './moniToring.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 
 
-
-export default function MoniTorint() {
+export default function MoniToring() {
     return <div className="monitoring">
+        <h1>생산모니터링</h1>
+
+        
+        <CircularProgressbar className="Chart" value={50} text={"50%"} />;
+    
+        
+        
 </div>
 }
