4d4d0c4b0ee8b7b0bc5c47c56513c83a94c6c6aa
sunford4927 <p01097220945@gamil.com>
2022-11-30 10:25:45 +0900

    lee

--- react-admin-dashboard/src/pages/monitoring/MoniToring.jsx
+++ react-admin-dashboard/src/pages/monitoring/MoniToring.jsx
@@ -1,18 +1,38 @@
-import React from "react";
-import './moniToring.css';
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-
-
+import React, { useEffect, useState } from "react";
+import "./moniToring.css";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import axios from "axios";
 
 export default function MoniToring() {
-    return <div className="monitoring">
-        <h1>생산모니터링</h1>
+    function roof_data(){
+        setInterval(function(){
+            // for(var i=0, i < production.length; i++){
+                // if();
+            // }
+            // 3초마다 실행
+        },3000)
 
+    }
+
+//   useEffect(() => {
+//     const Plansgetdata = async () => {
+//       try {
+//         var production = await axios.get('http://127.0.0.1:8000/production_log');
         
-        <CircularProgressbar className="Chart" value={50} text={"50%"} />;
-    
         
-        
-</div>
+
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+//     Plansgetdata();
+//   }, []);
+
+  return (
+    <div className="monitoring">
+      <h1>생산모니터링</h1>
+      <CircularProgressbar className="Chart" value={100} text={"50%"} />;
+    </div>
+  );
 }
