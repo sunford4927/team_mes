ede7c4cd8902821b4a3482f0f081f23e29da152d
sunford4927 <p01097220945@gamil.com>
2022-12-08 12:58:19 +0900

    생산등록계획수주 편집

--- react-admin-dashboard/src/pages/monitoring/MoniToring.jsx
+++ react-admin-dashboard/src/pages/monitoring/MoniToring.jsx
@@ -3,22 +3,25 @@ import "./moniToring.css";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import axios from "axios";
-import Chart from "./chart/Chart";
+import ProgressBar from "./chart/Chart";
+import { Card, CardBody, CardTitle, Table, FormGroup, Input } from "reactstrap"
 ////////////////////////////////////////////////////////
 // import 'bootstrap.css';
-//
+
+// function dic(result){
+//   var dic_list = []
+//   var moniter_dic = {}
+//   for(var u=0; u<result.length; u++){
+//     moniter_dic[u]['lot_num'] = result[u]['lot_num'];
+//     moniter_dic[u]['plan_name'] = result[u]['plan_name'];
+//     moniter_dic[u]['quantity'] = result[u]['quantity'];
+//     dic_list.push(moniter_dic)
+//     var moniter_dic = {}
+//   }
+// }
 
 export default function MoniToring(props) {
-  function math1(num, valuecount) {
-    return (num / valuecount) * 100;
-  }
-  function math2(num, valuecount) {
-    return (num / valuecount) * 100;
-  }
-  function math3(num, valuecount) {
-    return (num / valuecount) * 100;
-  }
-
+  
   const [num1, setNum1] = useState(0);
   // const [indata1, setIndata1] = useState();
   const [valuecount1, setValuecount1] = useState("0");
@@ -31,19 +34,42 @@ export default function MoniToring(props) {
   const [percent1, setPercent1] = useState(0);
   const [percent2, setPercent2] = useState(0);
   const [percent3, setPercent3] = useState(0);
+  const [result, setResult] = useState([]);
+  const testData = [
+    { bgcolor: "#6a1b9a", completed: Math.floor(percent1) },
+  ];
+
+  function math1(num, valuecount) {
+    return (num / valuecount) * 100;
+  }
+  function math2(num, valuecount) {
+    return (num / valuecount) * 100;
+  }
+  function math3(num, valuecount) {
+    return (num / valuecount) * 100;
+  }
+
+
   useEffect(() => {
     console.log("Test1");
     const outdata = async () => {
       try {
         const logdata = await axios.get("http://127.0.0.1:8000/productionlog/");
+        const result = await axios.get("http://127.0.0.1:8000/plans/");
+        // 원하는정보만 모아서 딕셔너리 구축
+        setResult(result.data)
+
+        // 데이터 0라인별로 모아서 저장
         var data_list1 = [];
         for (var i = 324; i < 450; i += 3) {
           data_list1.push(logdata.data[i]);
         }
+        // 데이터 1라인별로 모아서 저장        
         var data_list2 = [];
         for (var i = 325; i < 451; i += 3) {
           data_list2.push(logdata.data[i]);
         }
+        // 데이터 2라인별로 모아서 저장
         var data_list3 = [];
         for (var i = 326; i < 452; i += 3) {
           data_list3.push(logdata.data[i]);
@@ -88,37 +114,49 @@ export default function MoniToring(props) {
     setPercent3(math3(num3, valuecount3));
     console.log(percent3);
   }, [num3]);
-  
+console.log(testData[0]['completed'])
   return (
-    <div>
-    <div className="monitoring">
-      <div className="bar">
+    
+      <div className="monitoring">
+
+
+        <Table  className="no-wrap mt-3 align-middle table-table" responsive borderless striped bordered hover size="sm" variant="dark">
+            <thead>
+              <tr>
+                <th>NO</th>
+                <th>LOT번호</th>
+                <th className="table-score">생산명</th>
+                <th>계획수량</th>
+                <th>생산완료예정일</th>
+                <th>생산계획등록일</th>
+                <th>생산진행상태</th>
+              </tr>
+            </thead>
+            <tbody>
+              {result.map((tdata, index) => (
+                <tr key={index} className="border-top table-tr">
+                  <td>
+                    <div className="d-flex align-items-center p-2">
+                        <h6 className="mb-0">{index}</h6>
+                    </div>
+                  </td>
+                  <td>{tdata.lot_num}</td>
+                  <td>
+                    {tdata.plan_name}
+                  </td>
+                  <td>{tdata.quantity}</td>
+                  <td>{tdata.reg_date}</td>
+                  <td>{tdata.due_date}</td>
+                  <td>
+                    <ProgressBar key={index} bgcolor={testData[0]['bgcolor']} completed={testData[0]['completed']}/>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+
+
       </div>
-        <div className="chart_one">
-          <CircularProgressbar
-            className="Chart1"
-            value={percent1}
-            text={Math.floor(percent1) + "%"}
-          />
-        </div>
-        <div className="chart_two">
-          <CircularProgressbar
-            className="Chart2"
-            value={percent2}
-            text={Math.floor(percent2) + "%"}
-          />
-        </div>
-        <div className="chart_thr">
-          <CircularProgressbar
-            className="Chart3"
-            value={percent3}
-            text={Math.floor(percent3) + "%"}
-          />
-        </div>
-    </div>
-        <div>
-        <Chart num1={num1} num2={num2} num3={num3}/>
-        </div>
-    </div>
+  
   );
 }
